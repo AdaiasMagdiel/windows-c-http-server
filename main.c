f73e7eadebaf3b23c54f9c0d5a27838d90dd5c66
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-05-25 20:48:13 -0300

    feat: implement basic HTTP request parsing
    This commit implements basic parsing of HTTP requests, extracting the method and path from the received data.
    
    - Added `src/request.h` and `src/request.c` to handle request parsing.
    - Modified `main.c` to use the new `getRequest` function to parse incoming requests.
    - Removed redundant parsing code in `main.c`.

--- main.c
+++ main.c
@@ -3,83 +3,11 @@
 #include <string.h>
 #include "src/server.h"
 #include "src/content.h"
+#include "src/request.h"
 
-#define RECV_BUFFER_LENGTH 512
-
-typedef struct REQUEST {
-	char method[32];
-	char path[512];
-} REQUEST;
-
-REQUEST parseRecv(char *recvData) {
-	char method[32];
-	char path[512];
-
-    int start;
-	int end;
-	int ptr;
-    char c;
-
-    // Get method
-    start = 0;
-	end = 0;
-	ptr = 0;
-
-	while (1) {
-		c = recvData[ptr];
-
-		if (c == ' ') {
-			end = ptr;
-            break;
-		}
-
-        ptr++;
-	}
-
-    for (int i=start; i<end; i++) {
-        method[i] = recvData[i];
-    }
-    method[end] = '\0';
-
-    // Get path
-	start = 0;
-	end = 0;
-	ptr = 0;
-
-	while (1) {
-		c = recvData[ptr];
-
-        if(c == '?' && start > 0) {
-            end = ptr;
-            break;
-        }
-
-		if (c == ' ') {
-			if (start == 0) {
-				start = ptr+1;
-			} else {
-                end = ptr;
-                break;
-            }
-		}
-
-        ptr++;
-	}
-
-    for (int i=start; i<end; i++) {
-        path[i-start] = recvData[i];
-    }
-    path[end-start] = '\0';
-
-    REQUEST req;
-    strcpy(req.method, method);
-    strcpy(req.path, path);
-    
-    return req;
-}
 
 int main() {
-    char *htmlDir = "./public";
+    char file[512] = "./public";
 
     char *hostAddress = "0.0.0.0";
     int portNumber = 3000;
@@ -88,38 +16,22 @@ int main() {
 
     SOCKET clientSocket;
     while (1) {
-    	char buff[RECV_BUFFER_LENGTH];
-
         clientSocket = getConnection(serverSocket);
 
-        int res = recv(clientSocket, buff, RECV_BUFFER_LENGTH, 0);
-		if (res == SOCKET_ERROR) {
-			printf("Error: Unable to read recv.\n");
-			break;
-		}
-		REQUEST req = parseRecv(buff);
-
-		char htmlFilePath[512];
-		strcpy(htmlFilePath, htmlDir);
+        REQUEST req = getRequest(clientSocket);
 
 		if (strcmp(req.path, "/") == 0) {
-			strcat(htmlFilePath, "/index.html");
+			strcat(file, "/index.html");
 		} else {
-			strcat(htmlFilePath, req.path);
+			strcat(file, req.path);
 		}
-
-
-		// ----------------------------
-		// printf("-----------\nBuff: \n%s\n-----------\n", buff);
-		// printf("Method: >%s<\nPath: >%s<\n", req.method, req.path);
-		// ----------------------------
 
 		// req.method   ---   req.path
 		printf("\x1b[37m%s\x1b[0m   ---   \x1b[37m%s\x1b[0m", req.method, req.path);
 
-        char *htmlContent = getHTML(htmlFilePath);
+        char *content = getResponse(file);
         int bytes;
-        if (htmlContent == NULL) {
+        if (content == NULL) {
         	// req.method   ---   req.path   ---   404
         	printf("   ---   \x1b[31m404\x1b[0m");
 
@@ -128,9 +40,9 @@ int main() {
         	// req.method   ---   req.path   ---   200
 	        printf("   ---   \x1b[32m200\x1b[0m");
 
-	        bytes = sendData(clientSocket, htmlContent);
+	        bytes = sendData(clientSocket, content);
 
-	        free(htmlContent);
+	        free(content);
         }
 
         // req.method   ---   req.path   ---   int:status   ---   Bytes: int
