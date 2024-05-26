2f5a92680c2ab16e401a265ff06d733739ab355b
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-05-25 22:03:32 -0300

    feat: improve content handling
    This commit refactors the content handling, replacing the previous `getHTML` function with a more comprehensive `getResponse` function. This function now handles:
    
    - Retrieving the content of a file.
    - Constructing the HTTP header with appropriate status and content type.
    - Generating a 404 Not Found page if the file is not found.
    - Returning a `RESPONSE` struct containing the status code and the content.

--- src/content.c
+++ src/content.c
@@ -1,11 +1,9 @@
 #include "content.h"
+#include <string.h>
 
 // TODO: Add a log 
 
-char* getHTML(const char *filepath) {
-    const char *header = "HTTP/1.1 200 OK\r\nServer: Magotron2002\r\nContent-Type: text/html\r\n\r\n";
-    size_t headerLen = strlen(header);
-
+char* getFileContent(const char *filepath) {
     FILE *fp = fopen(filepath, "rb");
     if (fp == NULL) {
         // fprintf(stderr, "Error: Unable to open file.\n");
@@ -22,7 +20,7 @@ char* getHTML(const char *filepath) {
         return NULL;
     }
 
-    size_t totalSize = headerLen + (size_t)fileSize + 1;
+    size_t totalSize = (size_t)fileSize + 1;
     char *data = (char *)malloc(totalSize);
     if (data == NULL) {
         // fprintf(stderr, "Error: Unable to allocate memory.\n");
@@ -30,9 +28,7 @@ char* getHTML(const char *filepath) {
         return NULL;
     }
 
-    strcpy(data, header);
-
-    size_t bytesRead = fread(data + headerLen, 1, (size_t)fileSize, fp);
+    size_t bytesRead = fread(data, 1, (size_t)fileSize, fp);
     if (bytesRead != (size_t)fileSize) {
         // fprintf(stderr, "Error: Unable to read file content (read %zu bytes, expected %zu bytes).\n", bytesRead, (size_t)fileSize);
         free(data);
@@ -40,13 +36,98 @@ char* getHTML(const char *filepath) {
         return NULL;
     }
 
-    data[headerLen + (size_t)fileSize] = '\0';
+    data[(size_t)fileSize] = '\0';
 
     fclose(fp);
 
     return data;
 }
 
-char* getResponse(char *filepath) {
-    return getHTML(filepath);
+char* getHTTPHeader(const char *status, const char *contentType) {
+    if (status == NULL) status = "200 OK";
+    if (contentType == NULL) contentType = "text/html";
+
+    size_t headerSize = strlen("HTTP/1.1 ") + strlen(status) + strlen("\r\n") +
+                        strlen("Server: Magotron2002\r\n") +
+                        strlen("Content-Type: ") + strlen(contentType) + strlen("\r\n\r\n") + 1;
+
+    char *headerString = (char *)malloc(headerSize);
+    if (headerString == NULL) {
+        return NULL;
+    }
+
+    snprintf(headerString, headerSize, 
+        "HTTP/1.1 %s\r\n"
+        "Server: Magotron2002\r\n"
+        "Content-Type: %s\r\n"
+        "\r\n", 
+    status, contentType);
+
+    return headerString;
+}
+
+char* getFakeContentType(char *filepath, int is404) {
+    int len = strlen(filepath)+1;
+    int mark = 0;
+
+    for (int i=0; i<len; i++) {
+        if (filepath[i] == '.') mark = i;
+    }
+
+    char buffer[16];
+    for (int i=mark; i<len; i++) {
+        buffer[i-mark] = filepath[i];
+    }
+    buffer[len] = '\0';
+
+    char *contentType = (char *)malloc(sizeof(char) * 48);
+    if (contentType == NULL) return NULL;
+
+    if (is404) {
+        strcpy(contentType, "text/html");
+        return contentType;
+    }
+
+    if (strcmp(buffer, ".html") == 0) {
+        strcpy(contentType, "text/html");
+    } else if (strcmp(buffer, ".css") == 0){
+        strcpy(contentType, "text/css");
+    } else if (strcmp(buffer, ".js") == 0){
+        strcpy(contentType, "application/javascript");
+    } else {
+        strcpy(contentType, "text/plain");
+    }
+
+    return contentType;
+}
+
+RESPONSE getResponse(char *filepath) {
+    char *status = "200 OK";
+    int statusInt = 200;
+    char *fileContent = getFileContent(filepath);
+
+    char *contentType = getFakeContentType(filepath, fileContent == NULL);
+
+    if (fileContent == NULL) {
+        status = "404 Not Found";
+        statusInt = 404;
+        fileContent = "<h1 style=\"text-align: center; margin-block: 36px;\">404 Not Found</h1><hr><p style=\"text-align: center;\">Magotron2002</p>";
+    }
+
+    char *httpHeader =  getHTTPHeader(status, contentType);
+
+    int len = strlen(httpHeader) + strlen(fileContent);
+
+    char *res =  (char *)malloc(sizeof(char) * len + 1);
+    strcpy(res, httpHeader);
+    strcat(res, fileContent);
+
+    free(contentType);
+    free(httpHeader);
+
+    RESPONSE response;
+    response.status = statusInt;
+    response.content = res;
+
+    return response;
 }
--- src/content.h
+++ src/content.h
@@ -5,7 +5,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* getHTML(const char *filepath);
-char* getResponse(char *filepath);
+typedef struct RESPONSE {
+	int status;
+	char *content;
+} RESPONSE;
+
+char* getFileContent(const char *filepath);
+char* getHTTPHeader(const char *status, const char *contentType);
+char* getFakeContentType(char *filepath, int is404);
+RESPONSE getResponse(char *filepath);
 
 #endif  // MAG_CONTENT
