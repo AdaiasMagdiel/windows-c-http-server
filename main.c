13f490b0f35cb454b766aac83a5867e70916f5ab
Adaías Magdiel <adaiasmagdiell@gmail.com>
2025-02-07 12:22:19 -0300

    feat: add Request struct to manage clients requests

--- main.c
+++ main.c
@@ -25,11 +25,39 @@
 #define MAX_CONNECTIONS 32
 #define BUFFER_SIZE 4096
 
+typedef struct Request {
+  char *method;
+  char *path;
+} Request;
+
+void parseRequest(Request *req, StringBuilder sb) {
+  size_t lines_count;
+  StringBuilder *lines = sb_split_to_builders(&sb, "\r\n", &lines_count);
+
+  size_t met_path_count;
+  StringBuilder *met_path =
+      sb_split_to_builders(&lines[0], " ", &met_path_count);
+
+  req->method = malloc(met_path[0].length + 1);
+  strcpy(req->method, met_path[0].data);
+
+  req->path = malloc(met_path[1].length + 1);
+  strcpy(req->path, met_path[1].data);
+
+  sb_free_array(met_path, met_path_count);
+  sb_free_array(lines, lines_count);
+}
+
+void freeRequest(Request *req) {
+  free(req->method);
+  free(req->path);
+}
+
 int initializeWinsock() {
   WSADATA wsaData;
   int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
   if (result != 0) {
-    printf("[ERROR] WSAStartup failed: %d\n", result);
+    fprintf(stderr, "[ERROR] WSAStartup failed: %d\n", result);
     return -1;
   }
   return 0;
@@ -40,7 +68,7 @@ void cleanupWinsock() { WSACleanup(); }
 SOCKET createSocket() {
   SOCKET serverfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
   if (serverfd == INVALID_SOCKET) {
-    printf("[ERROR] Unable to create socket: %d\n", WSAGetLastError());
+    fprintf(stderr, "[ERROR] Unable to create socket: %d\n", WSAGetLastError());
     return INVALID_SOCKET;
   }
 
@@ -48,7 +76,8 @@ SOCKET createSocket() {
   if (setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR,
                  (const char *)&enableReuseAddr,
                  sizeof(enableReuseAddr)) == SOCKET_ERROR) {
-    printf("[ERROR] Unable to set SO_REUSEADDR: %d\n", WSAGetLastError());
+    fprintf(stderr, "[ERROR] Unable to set SO_REUSEADDR: %d\n",
+            WSAGetLastError());
     closesocket(serverfd);
     return INVALID_SOCKET;
   }
@@ -67,14 +96,14 @@ struct sockaddr_in createAddress() {
 int startServer(SOCKET serverfd, struct sockaddr_in addr) {
   int result = bind(serverfd, (SOCKADDR *)&addr, sizeof(addr));
   if (result == SOCKET_ERROR) {
-    printf("[ERROR] Unable to bind: %d\n", WSAGetLastError());
+    fprintf(stderr, "[ERROR] Unable to bind: %d\n", WSAGetLastError());
     closesocket(serverfd);
     return SOCKET_ERROR;
   }
 
   result = listen(serverfd, MAX_CONNECTIONS);
   if (result == SOCKET_ERROR) {
-    printf("[ERROR] Unable to listen: %d\n", WSAGetLastError());
+    fprintf(stderr, "[ERROR] Unable to listen: %d\n", WSAGetLastError());
     closesocket(serverfd);
     return SOCKET_ERROR;
   }
@@ -85,7 +114,7 @@ int startServer(SOCKET serverfd, struct sockaddr_in addr) {
 SOCKET acceptConnection(SOCKET serverfd) {
   SOCKET client = accept(serverfd, NULL, NULL);
   if (client == INVALID_SOCKET) {
-    printf("[ERROR] Unable to accept client: %d\n", WSAGetLastError());
+    fprintf(stderr, "[ERROR] Unable to accept client: %d\n", WSAGetLastError());
     return INVALID_SOCKET;
   }
   return client;
@@ -101,65 +130,61 @@ void closeServer(SOCKET serverfd) {
   closesocket(serverfd);
 }
 
-void free_split(char **strings, size_t length) {
-  for (size_t i = 0; i < length; i++) {
-    free(strings[i]);
-  }
-  free(strings);
+void sb_append_int(StringBuilder *sb, int value) {
+  char buffer[32];
+  snprintf(buffer, 32, "%d", value);
+
+  sb_append(sb, buffer);
 }
 
-void proccessResponse(StringBuilder *sb) {
-  printf("[Request]:\n%s\n", sb->data);
+void readFile(Request req, StringBuilder *headers, StringBuilder *body) {
+  char *file = req.path;
+  if (file[0] == '/') {
+    file++;
+  }
+  int res = sb_read_file(body, file);
 
-  size_t length;
-  char **lines = sb_split(sb, " ", &length);
+  if (res == -1) {
+    sb_reset(body);
 
-  sb_free(sb);
-  sb_init(sb);
-  sb_append(sb, lines[1]);
-  sb_trim(sb, "/");
-  free_split(lines, length);
+    sb_append(body, "<h1 style=\"text-align: center;\">404 - Resource \"");
+    sb_append(body, file);
+    sb_append(body, "\" Not Found</h1>"
+                    "<hr>"
+                    "<p style=\"text-align: center;\">The page or resource you "
+                    "are looking for could not be found.</p>");
 
-  char path[sb->length + 1];
-  strcpy(path, sb->data);
-
-  StringBuilder body;
-  sb_init(&body);
-
-  sb_free(sb);
-  sb_init(sb);
-
-  if (sb_read_file(&body, path) == -1) {
-    sb_free(&body);
-    sb_init(&body);
-    sb_append(&body, "<h1>Not Found</h1>");
-
-    char buffer[8];
-    itoa(body.length, buffer, 10);
-
-    sb_append(sb, "HTTP/1.1 404 Not Found\r\n");
-    sb_append(sb, "Content-Type: text/html\r\n");
-    sb_append(sb, "Content-Length: ");
-    sb_append(sb, buffer);
-    sb_append(sb, "\r\n\r\n");
-    sb_append(sb, body.data);
-    sb_append(sb, "\r\n");
-    sb_free(&body);
+    sb_append(headers, "HTTP/1.1 404 Not Found\r\n");
+    sb_append(headers, "Content-Type: text/html\r\n");
+    sb_append(headers, "Content-Length: ");
+    sb_append_int(headers, body->length);
 
     return;
   }
 
-  char buffer[8];
-  itoa(body.length, buffer, 10);
+  sb_append(headers, "HTTP/1.1 200 OK\r\n");
+  sb_append(headers, "Content-Type: text/plain\r\n");
+  sb_append(headers, "Content-Length: ");
+  sb_append_int(headers, body->length);
+}
 
-  sb_append(sb, "HTTP/1.1 200 OK\r\n");
-  sb_append(sb, "Content-Type: text/plain\r\n");
-  sb_append(sb, "Content-Length: ");
-  sb_append(sb, buffer);
-  sb_append(sb, "\r\n\r\n");
-  sb_append(sb, body.data);
-  sb_append(sb, "\r\n");
+void makeResponse(SOCKET client, Request req) {
+  StringBuilder headers;
+  sb_init(&headers);
 
+  StringBuilder body;
+  sb_init(&body);
+
+  readFile(req, &headers, &body);
+
+  char brk[] = "\r\n\r\n";
+  int size = strlen(brk);
+
+  send(client, headers.data, headers.length, 0);
+  send(client, brk, size, 0);
+  send(client, body.data, body.length, 0);
+
+  sb_free(&headers);
   sb_free(&body);
 }
 
@@ -172,14 +197,18 @@ void handleClient(SOCKET client) {
   if (bytesReceived > 0) {
     sb.data[bytesReceived] = '\0';
 
-    proccessResponse(&sb);
+    Request req;
+    parseRequest(&req, sb);
 
-    send(client, sb.data, sb.length, 0);
+    printf("Method: %s | Path: %s\n", req.method, req.path);
+
+    makeResponse(client, req);
+    freeRequest(&req);
     sb_free(&sb);
   } else if (bytesReceived == 0) {
     printf("Client disconnected.\n");
   } else {
-    printf("[ERROR] recv failed: %d\n", WSAGetLastError());
+    fprintf(stderr, "[ERROR] recv failed: %d\n", WSAGetLastError());
   }
 }
 
@@ -201,7 +230,7 @@ int main() {
     return 1;
   }
 
-  printf("Server listening on http://localhost:%d...\n", PORT);
+  printf("Server listening on \x1b[32mhttp://localhost:%d\x1b[0m\n", PORT);
 
   while (1) {
     SOCKET client = acceptConnection(serverfd);
@@ -211,7 +240,6 @@ int main() {
       return 1;
     }
 
-    printf("Client connected.\n");
     handleClient(client);
 
     closeConnection(client);
